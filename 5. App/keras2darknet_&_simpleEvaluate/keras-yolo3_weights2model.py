7e029ae2461f8b38ec528ee19e957e0f1f233135
Krone <X10090916@gmail.com>
2020-06-08 00:57:57 +0800

    ## 5. Web App ### 5.1 Keras to Darknet codes done

---
+++ 5. App/keras2darknet_&_simpleEvaluate/keras-yolo3_weights2model.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*- 
+import argparse
+import os
+
+import numpy as np 
+import keras 
+from keras.models import load_model 
+from keras.layers import Input
+
+from yolo3.model import yolo_body
+
+
+parser = argparse.ArgumentParser(description="qqwweee-Keras-yolo3's weights to model")
+parser.add_argument('-h5', dest='h5_path', default='model/trained_weights_final.h5', help="Path to keras weight *.h5")
+parser.add_argument('-cls', dest='classes_path', default='model/person_classes.txt', help="Path to class file")
+parser.add_argument('-ahr', dest='anchors_path', default='model/yolo_anchors.txt', help="Path to anchors file")
+
+
+
+
+def YOLOv3Model(input_shape, h5_path, class_names, anchors):
+    # Load model, or construct model and load weights.
+    # anyway, it's important for the class
+    num_anchors = len(anchors)
+    num_classes = len(class_names)
+    try:
+        yolo_model = load_model(h5_path, compile=False)
+    except:
+        yolo_model = yolo_body(Input(shape=(input_shape[0], input_shape[1], 3)), num_anchors // 3, num_classes)
+        yolo_model.load_weights(h5_path)  # make sure model, anchors and classes match
+    else:
+        assert yolo_model.layers[-1].output_shape[-1] == \
+               num_anchors / len(yolo_model.output) * (num_classes + 5), \
+            'Mismatch between model and given anchor and class sizes'
+
+    # 至此，Keras-YOLOv3 模型加载完毕
+    print('{} model, anchors, and classes loaded.'.format(h5_path))
+    return yolo_model
+
+
+def _get_class(classes_path):
+    classes_path = os.path.expanduser(classes_path)
+    with open(classes_path) as f:
+        class_names = f.readlines()
+    class_names = [c.strip() for c in class_names]
+    return class_names
+
+def _get_anchors(anchors_path):
+    anchors_path = os.path.expanduser(anchors_path)
+    with open(anchors_path) as f:
+        anchors = f.readline()
+    anchors = [float(x) for x in anchors.split(',')]
+    return np.array(anchors).reshape(-1, 2)
+
+
+def _main(args):
+    class_names = _get_class(args.classes_path)
+    anchors = _get_anchors(args.anchors_path)
+
+    input_shape = (416,416)
+    model = YOLOv3Model(input_shape, args.h5_path, class_names, anchors)
+    model.save('trained_model_final.h5')
+
+
+
+if __name__ == "__main__":
+    _main(parser.parse_args())
---
+++ 5. App/keras2darknet_&_simpleEvaluate/keras2darknet.py
@@ -0,0 +1,159 @@
+# -*- coding: utf-8 -*- 
+import argparse
+import os
+
+import numpy as np
+import keras
+from keras.models import load_model
+
+import keras.backend as K
+
+
+
+
+parser = argparse.ArgumentParser(description="Keras model *.h5 + *.cfg convert to Darknet *.weights!!!")
+parser.add_argument('-cfg', dest='cfg_path', default='yolo-person.cfg', help="Path to Darknet cfg")
+parser.add_argument('-mh5', dest='model_h5_path', default='trained_model_final.h5', help="Path to keras model *.h5")
+parser.add_argument('-sf', dest='save_file', default='yolov3-keras2darknet.weights', help='final save *.weights file')
+
+
+
+
+class WeightSaver(object):
+
+    def __init__(self, model_h5_path, save_file):
+
+        self.model = load_model(model_h5_path, compile=False)
+
+        self.layers = {weight.name:weight for weight in self.model.weights}
+        self.sess = K.get_session()
+        self.fhandle = open(save_file,'wb')
+        self._write_head()
+
+    def _write_head(self):
+        numpy_data = np.ndarray(shape=(3,),
+                          dtype='int32',
+                          buffer=np.array([0,2,0],dtype='int32') )
+        self.save(numpy_data)
+        numpy_data = np.ndarray(shape=(1,),
+                          dtype='int64',
+                          buffer=np.array([320000],dtype='int64'))
+        self.save(numpy_data)
+
+    def get_bn_layername(self,num):
+        layer_name = 'batch_normalization_{num}'.format(num=num)
+        bias = self.layers['{0}/beta:0'.format(layer_name)]
+        scale = self.layers['{0}/gamma:0'.format(layer_name)]
+        mean = self.layers['{0}/moving_mean:0'.format(layer_name)]
+        var = self.layers['{0}/moving_variance:0'.format(layer_name)]
+       
+        bias_np = self.get_numpy(bias)
+        scale_np = self.get_numpy(scale)
+        mean_np = self.get_numpy(mean)
+        var_np = self.get_numpy(var)
+        return bias_np,scale_np,mean_np,var_np
+
+    def get_convbias_layername(self,num):
+        layer_name = 'conv2d_{num}'.format(num=num)
+        bias = self.layers['{0}/bias:0'.format(layer_name)]
+      
+        bias_np = self.get_numpy(bias)
+        return bias_np
+
+    def get_conv_layername(self,num):
+        layer_name = 'conv2d_{num}'.format(num=num)
+        conv = self.layers['{0}/kernel:0'.format(layer_name)]
+       
+        conv_np = self.get_numpy(conv)
+        return conv_np
+
+    def get_numpy(self,layer_name):
+        numpy_data = self.sess.run(layer_name)
+        return numpy_data
+
+    def save(self,numpy_data):
+        bytes_data = numpy_data.tobytes()
+        self.fhandle.write(bytes_data)
+        self.fhandle.flush()
+
+    def close(self):
+        self.fhandle.close()
+
+
+class KerasParser(object):
+
+    def __init__(self, cfg_path, model_h5_path, save_file):
+        self.block_gen = self._get_block(cfg_path)
+        self.weights_saver = WeightSaver(model_h5_path, save_file)
+        self.count_conv = 0
+        self.count_bn = 0
+
+    def _get_block(self,cfg_path):
+
+        block = {}
+        with open(cfg_path,'r', encoding='utf-8') as fr:
+            for line in fr:
+                line = line.strip()
+                if '[' in line and ']' in line:
+                    if block:
+                        yield block
+                    block = {}
+                    block['type'] = line.strip(' []')
+                elif not line or '#' in line:
+                    continue
+                else:
+                    key,val = line.strip().replace(' ','').split('=')
+                    key,val = key.strip(), val.strip()
+                    block[key] = val
+
+            yield block
+
+    def close(self):
+        self.weights_saver.close()
+
+    def conv(self, block):
+        self.count_conv += 1
+        batch_normalize = 'batch_normalize' in block
+        # print('handing.. ',self.count_conv)
+
+        # 如果bn存在，则先处理bn，顺序为bias，scale，mean，var
+        if batch_normalize:
+            bias,scale,mean,var = self.bn()
+            self.weights_saver.save(bias)
+            
+            scale = scale.reshape(1,-1)
+            mean = mean.reshape(1,-1)
+            var = var.reshape(1,-1)
+            remain = np.concatenate([scale,mean,var],axis=0)
+            self.weights_saver.save(remain)
+
+        # 否则，先处理biase
+        else:
+            conv_bias = self.weights_saver.get_convbias_layername(self.count_conv)
+            self.weights_saver.save(conv_bias)
+
+        # 接着处理weights
+        conv_weights = self.weights_saver.get_conv_layername(self.count_conv)
+        # 需要将(height, width, in_dim, out_dim)转换成(out_dim, in_dim, height, width)
+        conv_weights = np.transpose(conv_weights,[3,2,0,1])
+        self.weights_saver.save(conv_weights)
+
+    def bn(self):
+        self.count_bn += 1
+        bias,scale,mean,var = self.weights_saver.get_bn_layername(self.count_bn) 
+        return bias,scale,mean,var
+
+
+
+def _main(args):
+    keras_loader = KerasParser(args.cfg_path, args.model_h5_path, args.save_file)
+
+    for block in keras_loader.block_gen:
+        if 'convolutional' in block['type']:
+            keras_loader.conv(block)
+    keras_loader.close()
+
+
+
+if __name__ == '__main__':
+    _main(parser.parse_args())
